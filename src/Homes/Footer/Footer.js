eb885970498a7084b289effbc7725f11d28abf41
Alamgir4597 <rrony45@gmail.com>
2022-05-08 22:32:21 +0600

    update

--- src/Homes/Footer/Footer.js
+++ src/Homes/Footer/Footer.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className='container-fluid bg-dark mt-5 mb-3'>
+            <div className="row">
+                <div class="col">
+                    <p className='font-italic  text-light p-3 '>© 2022 Car House</p> 
+                </div>
+                <div className="col">
+                    <h6 className='text-light'>Contact Us</h6>
+                    <p className='font-italic  text-light '>
+                        Email:<br/>
+                        Facebook <br/>
+                        twitter
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
